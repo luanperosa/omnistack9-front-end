5d8ad7d80961aa134dc4fe62b8d4c44f65d7e44b
luanperosa <luan.perosa@hotmail.com>
2019-11-14 03:16:15 -0300

    add .env

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
-
 export default function Login({ history }) {
   const [ email, setEmail ] = useState('');
   
   async function handleSubmit(event) {
     event.preventDefault();
-
+    
     const response = await api.post('/sessions', { email } );
 
     const { _id } = response.data;
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-const dotenv = require('dotenv');
 
 const api = axios.create({
-  baseURL: process.env.URL_API
+  baseURL: process.env.REACT_APP_URL_API
 });
 
 export default api;
